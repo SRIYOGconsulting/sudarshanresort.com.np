82f17b7eb934a80e3d75887c513419a2a2504b2e
santosh shah <santosshah95@gmail.com>
2025-11-30 14:41:25 +0545

    fix textwriter animation and addd button below

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Homepage  from '../src/components/Home_page.jsx'
 import NavBar from './components/Nav_Bar.jsx'
 import SudarshanHome from './components/sudarshan_main.jsx'
 import ContentOverImg from './components/Content_Over_Img.jsx'
+
 const App = () => {
   return (
     <div>
