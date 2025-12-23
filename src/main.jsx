eb98e48ac2edf9948fc58cda299c051325a27c0b
PRACAS Upreti <100864380+pracasupreti@users.noreply.github.com>
2025-12-23 22:17:47 +0545

    moved to root

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,21 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
+import { BrowserRouter } from 'react-router-dom';
+import { HelmetProvider } from "react-helmet-async";
 
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+if (import.meta.env.MODE !== 'production') {
+  import('react-axe').then(axe => {
+    const ReactDOM = import('react-axe');
+    axe.default(React, ReactDOM, 1000);
+  });
+}
 
 createRoot(document.getElementById('root')).render(
-  <>
-  <App />
-  </>
-)
+  <HelmetProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </HelmetProvider>
+);
